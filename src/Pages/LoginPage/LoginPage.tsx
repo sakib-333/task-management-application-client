866074111474c9a0b2884bdf540cf8858fa94cc2
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-02-20 23:13:32 +0600

    Add login page.

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,46 @@
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <div className="card bg-background w-full max-w-sm shrink-0 shadow-2xl">
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn bg-primary hover:bg-secondary">Login</button>
+          </div>
+          <div className="divider"></div>
+          <button className="btn" type="button">
+            Sign in Google
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
