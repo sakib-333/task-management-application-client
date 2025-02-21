e98e50628aef96ea294cee3418955cf8949e1546
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-02-21 23:25:10 +0600

    Bug fix.

--- src/Modals/TaskDetailsModal/TaskDetailsModal.tsx
+++ src/Modals/TaskDetailsModal/TaskDetailsModal.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useRef } from "react";
 import useAxiosPublic from "../../Hooks/useAxiosPublic/useAxiosPublic";
+import { LuClipboardType } from "react-icons/lu";
+import { IoTime } from "react-icons/io5";
 
 const TaskDetailsModal = ({ id, setShowDetailsModal }: any) => {
   const axiosPublic = useAxiosPublic();
@@ -24,6 +26,15 @@ const TaskDetailsModal = ({ id, setShowDetailsModal }: any) => {
       <div className="modal-box">
         <h3 className="font-bold text-lg">{myTask?.title}</h3>
         <p>{myTask?.description}</p>
+        <div className="divider"></div>
+        <div className="flex items-center justify-between">
+          <p className="flex items-center">
+            <LuClipboardType /> <span>{myTask?.category}</span>
+          </p>
+          <p className="flex items-center">
+            <IoTime /> <span>{myTask?.timestamp}</span>
+          </p>
+        </div>
 
         <div className="modal-action">
           <button className="btn" onClick={() => setShowDetailsModal(false)}>
