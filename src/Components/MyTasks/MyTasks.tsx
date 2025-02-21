0695ebd76f19b02382b9c94adce1bbcc632aeca2
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-02-21 22:43:05 +0600

    Show all tasks.

--- src/Components/MyTasks/MyTasks.tsx
+++ src/Components/MyTasks/MyTasks.tsx
@@ -2,6 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../Hooks/useAxiosPublic/useAxiosPublic";
 import TaskCard from "../TaskCard/TaskCard";
 
+type Task = {
+  _id: string;
+  title: string;
+  description: string;
+  category: "To-Do" | "In Progress" | "Done";
+};
+
 const MyTasks = () => {
   const axiosPublic = useAxiosPublic();
   const { data: myTasks = [] } = useQuery({
@@ -11,23 +18,35 @@ const MyTasks = () => {
       return res.data;
     },
   });
-  console.log(myTasks);
+
+  const toDos = myTasks.filter((task: Task) => task.category === "To-Do");
+  const inProgress = myTasks.filter(
+    (task: Task) => task.category === "In Progress"
+  );
+  const done = myTasks.filter((task: Task) => task.category === "Done");
+
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
-      <div>
+      <div className="w-full">
         <h1 className="font-bold text-center text-primary">To-Do</h1>
         <div className="divider"></div>
-        <TaskCard />
+        {toDos.map((task: Task) => (
+          <TaskCard key={task._id} task={task} />
+        ))}
       </div>
-      <div>
+      <div className="w-full">
         <h1 className="font-bold text-center text-primary">In Progress</h1>
         <div className="divider"></div>
-        <TaskCard />
+        {inProgress.map((task: Task) => (
+          <TaskCard key={task._id} task={task} />
+        ))}
       </div>
-      <div>
+      <div className="w-full">
         <h1 className="font-bold text-center text-primary">Done</h1>
         <div className="divider"></div>
-        <TaskCard />
+        {done.map((task: Task) => (
+          <TaskCard key={task._id} task={task} />
+        ))}
       </div>
     </div>
   );
--- src/Components/TaskCard/TaskCard.tsx
+++ src/Components/TaskCard/TaskCard.tsx
@@ -1,20 +1,25 @@
 import { FaEye, FaRegEdit } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 
-const TaskCard = () => {
+type Task = {
+  _id: string;
+  title: string;
+  description: string;
+  category: "To-Do" | "In Progress" | "Done";
+};
+
+interface TaskCardProps {
+  task: Task;
+}
+
+const TaskCard = ({ task }: TaskCardProps) => {
   return (
-    <div className="max-w-sm w-full h-full p-3 max-h-[135px] border rounded-md group">
-      <h1 className="font-bold truncate">Test Task</h1>
-      <p className=" text-justify max-h-[75px] overflow-hidden">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
-        consequuntur voluptatum similique placeat. Ea, itaque iste. Nobis
-        corporis culpa hic doloremque, voluptate enim sequi dignissimos corrupti
-        nemo aspernatur delectus consequatur. Lorem ipsum dolor, sit amet
-        consectetur adipisicing elit. Pariatur consequuntur voluptatum similique
-        placeat. Ea, itaque iste. Nobis corporis culpa hic doloremque, voluptate
-        enim sequi dignissimos corrupti nemo aspernatur delectus consequatur.
+    <div className="max-w-sm mb-2 mx-auto w-full h-full p-3 max-h-[80px] border rounded-md group">
+      <h1 className="text-xs font-bold truncate">{task.title}</h1>
+      <p className="text-xs text-justify max-h-[30px] overflow-hidden">
+        {task.description}
       </p>
-      <div className="gap-2 hidden group-hover:flex">
+      <div className="text-xs gap-2 mt-1 hidden group-hover:flex">
         <button>
           <FaEye />
         </button>
